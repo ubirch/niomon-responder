4b1dd8f8ca2b5f20a452873fede9c73a7d68ed4b
mrobakowski <mikolaj.robakowski@ubirch.com>
2019-04-25 19:26:41 +0200

    [UP-515] responder now handles successful responses, unauthorized errors, and passes other errors through

--- src/main/scala/com/ubirch/responder/ResponderMicroservice.scala
+++ src/main/scala/com/ubirch/responder/ResponderMicroservice.scala
@@ -1,14 +1,19 @@
 package com.ubirch.responder
 
+import java.nio.charset.StandardCharsets.UTF_8
+
 import com.ubirch.niomon.base.NioMicroservice
 import com.ubirch.kafka._
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.header.internals.RecordHeader
 
 import scala.collection.JavaConverters._
 
 class ResponderMicroservice extends NioMicroservice[Array[Byte], Array[Byte]]("responder") {
+
   import ResponderMicroservice._
+
   private val NormalTopic = topicSet(config.getStringList("normal-input-topics").asScala)
   private val ErrorTopic = topicSet(config.getStringList("error-input-topics").asScala)
 
@@ -20,11 +25,36 @@ class ResponderMicroservice extends NioMicroservice[Array[Byte], Array[Byte]]("r
   }
 
   def handleNormal(topic: String, record: ConsumerRecord[String, Array[Byte]]): ProducerRecord[String, Array[Byte]] = {
-    record.toProducerRecord(onlyOutputTopic)
+    // TODO: decide on what data should be included here
+    //  also, the source of `record`s here is message-signer right now, so we lose the context - should something be
+    //  done about that?
+    record.toProducerRecord(
+      topic = onlyOutputTopic,
+      value = """{"message": "your request has been submitted"}""".getBytes(UTF_8)
+    )
   }
 
   def handleError(topic: String, record: ConsumerRecord[String, Array[Byte]]): ProducerRecord[String, Array[Byte]] = {
-    record.toProducerRecord(onlyOutputTopic)
+    val headers = record.headersScala
+    logger.debug(s"record headers: ${record.headersScala}")
+    headers.get("http-status-code") match {
+      // Special handling for unauthorized, because we don't handle that one via NioMicroservice error handling
+      // (Q: maybe we should? but that would prevent us to easily do something with unauthorized, but otherwise valid
+      // packets)
+      case Some("401") => record.toProducerRecord(
+        topic = onlyOutputTopic,
+        value = stringifyException(UnauthorizedException, record.key()).getBytes(UTF_8)
+      )
+      case None =>
+        logger.warn("Someone's not using NioMicroservice and forgot to attach `http-status-code` header to their " +
+          s"error message. Message: [requestId = ${record.key()}, headers = $headers]")
+        val httpStatusCodeHeader = new RecordHeader("http-status-code", "500".getBytes(UTF_8))
+        record.toProducerRecord(
+          topic = onlyOutputTopic,
+          headers = (record.headers().toArray :+ httpStatusCodeHeader).toIterable.asJava
+        )
+      case _ => record.toProducerRecord(onlyOutputTopic)
+    }
   }
 }
 
@@ -37,4 +67,7 @@ object ResponderMicroservice {
 
     TopicSet
   }
+
+  object UnauthorizedException extends Exception("Unauthorized!")
+
 }
